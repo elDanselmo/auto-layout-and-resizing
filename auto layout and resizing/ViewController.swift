1e36c9722b0f7885d902c086559105528eede290
Antonino D'Anselmo <danselmo.antonino@gmail.com>
2022-02-01 22:27:17 +0100

    Initial Commit

---
+++ auto layout and resizing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto layout and resizing
+//
+//  Created by Antonino D'Anselmo on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
